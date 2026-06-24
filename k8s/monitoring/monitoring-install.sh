a845ecfabe68cc641223245567cb135f80cca042
Benjamin Harris <beh@nemlig.com>
2026-06-25 00:34:50 +0200

    update resource metrics grafana

--- k8s/monitoring/monitoring-install.sh
+++ k8s/monitoring/monitoring-install.sh
@@ -3,11 +3,14 @@ set -e
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
+helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
   --namespace monitoring \
   --create-namespace \
   --set grafana.service.type=NodePort \
   --set grafana.service.nodePort=30030 \
   --set grafana.adminPassword=admin \
   --set grafana.defaultDashboardsEnabled=false \
+  --set kubelet.enabled=true \
+  --set kubelet.serviceMonitor.cAdvisor=true \
+  --set prometheus.prometheusSpec.kubeletScrapeTimeout=30s \
   --wait --timeout 5m
